e5fa4d2763cc00ae5005b65737dad19969142b94
toma9512 <gmldnjs985@gmail.com>
2025-10-28 20:46:51 +0900

    Lv2 : 피보나치 수

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Solution sl = new Solution();
+        System.out.println(sl.solution(23));
+    }
+}
 
+class Solution {
+    public int solution(int n) {
+        // 종료 시점
+        if (n==0) return 0;
+        if (n==1) return 1;
+
+        return (solution(n-1)+solution(n-2))%1234567;
     }
 }
