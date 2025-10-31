e6e6f99e26fc1b6a88db23f641e4f1af730d61be
toma9512 <gmldnjs985@gmail.com>
2025-10-31 17:46:00 +0900

    Lv2 : 방문 길이

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        List<int[]> list = new ArrayList<>();
+        int[] arr1 = new int[2];
+        int[] arr2 = new int[2];
+        list.add(arr1);
+        System.out.println(""+1+2);
     }
 }
 
