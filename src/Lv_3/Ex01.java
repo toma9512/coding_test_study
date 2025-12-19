164951512fa1428af0418638a9268e188c6c1923
heewon <heewon@heewonui-MacBookAir.local>
2025-12-19 20:05:22 +0900

    Lv3: 정수 삼각형

---
+++ src/Lv_3/Ex01.java
@@ -0,0 +1,32 @@
+package Lv_3;
+
+/*
+문제 설명
+위와 같은 삼각형의 꼭대기에서 바닥까지 이어지는 경로 중, 거쳐간 숫자의 합이 가장 큰 경우를 찾아보려고 합니다. 아래 칸으로 이동할 때는 대각선 방향으로 한 칸 오른쪽 또는 왼쪽으로만 이동 가능합니다. 예를 들어 3에서는 그 아래칸의 8 또는 1로만 이동이 가능합니다.
+삼각형의 정보가 담긴 배열 triangle이 매개변수로 주어질 때, 거쳐간 숫자의 최댓값을 return 하도록 solution 함수를 완성하세요.
+
+제한사항
+삼각형의 높이는 1 이상 500 이하입니다.
+삼각형을 이루고 있는 숫자는 0 이상 9,999 이하의 정수입니다.
+
+ */
+
+public class Ex01 {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        int n = triangle.length;
+        int[][] dp = new int[n+1][n+1];
+
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=i; j++) {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-1]) + triangle[i-1][j-1];
+            }
+        }
+
+        for (int i=1; i<=n; i++) {
+            answer = Math.max(answer, dp[n][i]);
+        }
+
+        return answer;
+    }
+}
