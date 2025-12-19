4f366305d49a7ae0c933432cc5966117c1ebc06d
heewon <heewon@heewonui-MacBookAir.local>
2025-12-19 21:20:49 +0900

    Lv3: 이중우선순위큐

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "1.-4";
-        String[] arr = s.split(".");
+        PriorityQueue<Integer> pq = new PriorityQueue<>(((o1, o2) -> o2 - o1));
+        pq.add(3);
+        pq.add(1);
+        pq.add(4);
+        System.out.println(pq);
+        System.out.println(pq);
+
+
     }
 }
 
